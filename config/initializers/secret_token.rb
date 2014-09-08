cb3aee4d2d6dc6f66972aad922f915c562533345
Brett Lilley <brettlilley.j@gmail.com>
2014-09-08 14:24:40 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracApp::Application.config.secret_key_base = 'be2150b1b8dd9211e0226262df01d8dce20a0968089ae4a962349be0d078f3796dcd9189037790d7b1c9d55a80275126b4a3d4f2b91786ef360315b3f82c8bdc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PracApp::Application.config.session_store :cookie_store, key: '_prac_app_session'
